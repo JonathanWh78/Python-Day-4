ae2c5debb927799897f5cc95f88b061d028cc615
jonathanwh <jonathanwh78@gmail.com>
2022-01-27 11:44:07 +0000

    commit

--- test_QAFuncs.py
+++ test_QAFuncs.py
@@ -4,7 +4,7 @@ def test_fact():
     assert QACommsTestFunc.fact(3) == 6
 
 def test_list_of_squares():
-    assert QACommsTestFunc.list_of_squares(1) == 1
+    assert QACommsTestFunc.list_of_squares(1) == "1: 1"
 
 def test_vowels():
     assert QACommsTestFunc.vowels("letter") == "ee"
