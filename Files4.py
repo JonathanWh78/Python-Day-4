71b47878d0a677bf086be5a6ece648d3daef6ded
jonathanwh <jonathanwh78@gmail.com>
2022-01-27 09:56:08 +0000

    commit

---
+++ Files4.py
@@ -0,0 +1,23 @@
+#working code to remove all from readme and replace with the same but upper
+
+openedfile = open("README.md")
+readfile = openedfile.read()
+print (readfile)
+openedfile.close()
+
+upper = readfile.upper() #can be interchanged with upper or lower
+openedfile = open("README.md","w")
+openedfile.write(upper)
+openedfile.close()
+
+
+
+# #inserting files
+# openedfile = open("README.md")
+# readfile = openedfile.readlines()
+# print (readfile)
+# openedfile.close()
+# readfile.insert(8,"\ninserted line\n")
+# openedfile = open("README.md","w")
+# openedfile.writelines(readfile)
+# openedfile.close()
