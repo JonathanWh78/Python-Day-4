ff0b4ecb441935e5bc59918f09f8eed4fc2104c6
jonathanwh <jonathanwh78@gmail.com>
2022-01-27 12:34:21 +0000

    testing done

--- test_QAFuncs.py
+++ test_QAFuncs.py
@@ -4,7 +4,7 @@ def test_fact():
     assert QACommsTestFunc.fact(3) == 6
 
 def test_list_of_squares():
-    assert QACommsTestFunc.list_of_squares(1) == {1: 1}
+    assert QACommsTestFunc.list_of_squares(2) == {1: 1, 2: 4}
 
 def test_vowels():
     assert QACommsTestFunc.vowels("letter") == 2
